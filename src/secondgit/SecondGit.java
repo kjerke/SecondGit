bc10d6ba085e1ccdf9f72363cf587347c7292526
kevinjerke <kevinjerke@10.30.17.51>
2017-01-25 15:24:21 -0600

    Modified main method.

--- src/secondgit/SecondGit.java
+++ src/secondgit/SecondGit.java
@@ -16,6 +16,8 @@ public class SecondGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello Git");
     }
     
 }
